54cdfb4ed845997a85f2d5826f116b31ed91c0cc
yuriykolomiyets <juriy121@gmail.com>
2023-02-18 16:47:43 +0100

    1

--- src/main/java/com/example/demo/service/TroopServiceImpl.java
+++ src/main/java/com/example/demo/service/TroopServiceImpl.java
@@ -3,10 +3,7 @@ package com.example.demo.service;
 import com.example.demo.model.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Random;
 
 @Service
@@ -34,9 +31,17 @@ public class TroopServiceImpl {
     }
 
     /**
+     * This method generates an array of troop amounts for a given total amount and a specified number of troop types.
+     * @param amount represents the total number of troops to generate
+     * @return Array of integers representing the number of troops for each troop type
      *
-     * @param amount
-     * @return Array of amounts of troops for each type
+     * The first step to calculate the maximum number of troops per type, which is maxAmountPerType.
+     * Next, create an array of typesAmount integers called troopsAmount to store the final number of troops for
+     * each type.
+     * The variable totalAmount will keep track of the total number of troops was assigned.
+     * The loop iterates over each troop type. For each type, method calculates the max number of troops
+     * that can be assigned. The Math.min function is used to ensure that the maximum amount does not exceed
+     * maxAmountPerType.
      */
 
     private Integer[] troopsAmountGenerate(Integer amount) {
--- src/test/java/com/example/demo/service/TroopServiceImplTest.java
+++ src/test/java/com/example/demo/service/TroopServiceImplTest.java
@@ -9,6 +9,7 @@ import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,7 +25,19 @@ class TroopServiceImplTest {
 
     @Test
     void get() {
+        Integer amount = 10;
+        Army army = troopService.get(amount);
+        Map<Troop, Integer> army1 = army.getArmy();
 
+        // check that all troops.getAmount will be not equals to 0
 
+        assertTrue(army1.entrySet().stream().allMatch(entry -> entry.getValue() != 0));
+
+        // check that sum of all troops.getAmount will be equals to amount
+
+        Integer totalAmount = army1.values().stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+        assertEquals(amount, totalAmount);
     }
 }
